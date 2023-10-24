d59d79f8a81b829f85298132c96f1bf8b2968658
jzaldivar <jorge.zaldivar@gmail.com>
2023-10-23 20:20:31 -0600

    Remove solution

--- contrasena.py
+++ contrasena.py
@@ -1,34 +1,15 @@
 """
-Verificar contraseña.
+Inserta el encabezado aquí y escribe tu código abajo
 """
 
-def verificar(contrasena):
-    "Verificar que la contraseña cumpla los requisitos."
-    error = False
-    # Tiene una longitud de, al menos, diez caracteres
-    if len(contrasena) < 10:
-        error = True
-    # Contiene, al menos, dos letras mayúsculas
-    mayus = 0
-    for letra in contrasena:
-        # ¿Es letra?
-        if letra.upper() != letra.lower():
-            # ¿Es mayúscula?
-            if letra == letra.upper():
-                mayus += 1
-    if mayus < 2:
-        error = True
-    # No comienza con un número
-    if contrasena[0] in "0123456789":
-        error = True
-    return not error
-
+# Declaraciones
+CONSTANTE = valor
 
 # Entradas
-contrasena = input("Contraseña: ")
+entrada = input()
+
+# Proceso
+salida = entrada
 
 # Salidas
-if verificar(contrasena):
-    print("La contraseña sí cumple con los requisitos")
-else:
-    print("La contraseña no cumple con los requisitos")
+print(salida)
