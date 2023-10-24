8565d17d786b11e972e1f5d19e0fb458201a6d3f
jzaldivar <jorge.zaldivar@gmail.com>
2023-10-23 19:54:28 -0600

    Code solution Generate autograding.json

--- contrasena.py
+++ contrasena.py
@@ -1,15 +1,34 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Verificar contraseña.
 """
 
-# Declaraciones
-CONSTANTE = valor
+def verificar(contrasena):
+    "Verificar que la contraseña cumpla los requisitos."
+    error = False
+    # Tiene una longitud de, al menos, diez caracteres
+    if len(contrasena) < 10:
+        error = True
+    # Contiene, al menos, dos letras mayúsculas
+    mayus = 0
+    for letra in contrasena:
+        # ¿Es letra?
+        if letra.upper() != letra.lower():
+            # ¿Es mayúscula?
+            if letra = letra.upper():
+                mayus += 1
+    if mayus < 2:
+        error = True
+    # No comienza con un número
+    if contrasena[0] in "0123456789":
+        error = True
+    return not error
+
 
 # Entradas
-entrada = input()
-
-# Proceso
-salida = entrada
+contrasena = input("Contraseña: ")
 
 # Salidas
-print(salida)
+if verificar(contrasena):
+    print("La contraseña sí cumple con los requisitos")
+else:
+    print("La contraseña no cumple con los requisitos")
